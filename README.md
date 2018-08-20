cf60526b0fc6c0f9e90b68605c0f125d499c7586
Carlos <A01209474@itesm.mx>
2018-08-20 10:31:21 -0500

    first attempt demo calculator

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DemoCalculatorAI
 Demo assignment 2018
+
+
+# Create a run script
+
+gcc demoCalculator.c -o program
---
+++ demoCalculator.c
@@ -0,0 +1,15 @@
+#include <stdlib.h> 
+#include <stdio.h> 
+
+int main(int argc, char const *argv[]) {
+    int a = 0, b = 0, result;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    result = a + b;
+
+    printf("%i", result);
+
+    return 0;
+}
